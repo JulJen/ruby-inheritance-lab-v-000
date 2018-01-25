3696c14de36815126425518edbc009b3c02a4a3f
Jul Jen-Prog <juliajen.prog@gmail.com>
2018-01-25 20:38:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,17 +5,11 @@ class Student <  User
   attr_accessor :first_name, :last_name
 
   def initialize
-    # (first_name)
-    @first_name = first_name
-    @last_name = last_name
     @knowledge = []
   end
 
   def learn(know)
     @knowledge << know
-    know.knowledge
-    # knowledge.student = self
-    # @knowledge.collect { |info| info.student }
   end
 
 end
