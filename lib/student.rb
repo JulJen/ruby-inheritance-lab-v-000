7c87cff4d763fb1d488aec4a154b43e1ac9f0f19
Jul Jen-Prog <juliajen.prog@gmail.com>
2018-01-25 20:29:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student < Teacher
 
   def learn(knowledge)
     @knowledge << knowledge
-    return @knowledge.self
+    return @knowledge
     # @knowledge.collect { |info| info.student }
   end
 
