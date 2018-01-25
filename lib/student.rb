dac3c7d3448666db2f7d2b028c3ae3cd9fca161d
Jul Jen-Prog <juliajen.prog@gmail.com>
2018-01-25 20:04:23 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,11 +2,12 @@ require_relative "./teacher.rb"
 
 class Student < Teacher
 
-  attr_accessor :first_name
+  attr_accessor :first_name, :last_name
 
   def initialize
     # (first_name)
     @first_name = first_name
+    @last_name = last_name
   end
 
 end
